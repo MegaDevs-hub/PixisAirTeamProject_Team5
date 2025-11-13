64d944b641a568583aa13aefea629c30cc9500de
MegaDevs <waupoosa1@mail.gtc.edu>
2025-11-12 19:45:01 -0600

    Initial project setup

---
+++ PixisAirTeamProject/PixisAirMain.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace PixisAirTeamProject
+{
+    partial class PixisAirMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PixisAirMain));
+            this.paperPlanePictureBox = new System.Windows.Forms.PictureBox();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.employeeFormToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.jobFormToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.flightFormToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.employeeFormButton = new System.Windows.Forms.Button();
+            this.jobFormButton = new System.Windows.Forms.Button();
+            this.flightFormButton = new System.Windows.Forms.Button();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.greenAccentLabel = new System.Windows.Forms.Label();
+            this.orangeAccentLabel = new System.Windows.Forms.Label();
+            this.pinkAccentLabel = new System.Windows.Forms.Label();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.paperPlanePictureBox)).BeginInit();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // paperPlanePictureBox
+            // 
+            this.paperPlanePictureBox.Image = global::PixisAirTeamProject.Properties.Resources.PixisAirPaperPlane;
+            this.paperPlanePictureBox.Location = new System.Drawing.Point(585, 29);
+            this.paperPlanePictureBox.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.paperPlanePictureBox.Name = "paperPlanePictureBox";
+            this.paperPlanePictureBox.Size = new System.Drawing.Size(203, 195);
+            this.paperPlanePictureBox.TabIndex = 0;
+            this.paperPlanePictureBox.TabStop = false;
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip.TabIndex = 1;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.employeeFormToolStripMenuItem,
+            this.jobFormToolStripMenuItem,
+            this.flightFormToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuToolStripMenuItem.Font = new System.Drawing.Font("Franklin Gothic Medium", 9F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuToolStripMenuItem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // employeeFormToolStripMenuItem
+            // 
+            this.employeeFormToolStripMenuItem.Font = new System.Drawing.Font("Franklin Gothic Medium", 9F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.employeeFormToolStripMenuItem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
+            this.employeeFormToolStripMenuItem.Name = "employeeFormToolStripMenuItem";
+            this.employeeFormToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.employeeFormToolStripMenuItem.Text = "Employee Form";
+            // 
+            // jobFormToolStripMenuItem
+            // 
+            this.jobFormToolStripMenuItem.Font = new System.Drawing.Font("Franklin Gothic Medium", 9F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.jobFormToolStripMenuItem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
+            this.jobFormToolStripMenuItem.Name = "jobFormToolStripMenuItem";
+            this.jobFormToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.jobFormToolStripMenuItem.Text = "Job Form";
+            // 
+            // flightFormToolStripMenuItem
+            // 
+            this.flightFormToolStripMenuItem.Font = new System.Drawing.Font("Franklin Gothic Medium", 9F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.flightFormToolStripMenuItem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
+            this.flightFormToolStripMenuItem.Name = "flightFormToolStripMenuItem";
+            this.flightFormToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.flightFormToolStripMenuItem.Text = "Flight Form";
+            // 
+            // employeeFormButton
+            // 
+            this.employeeFormButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
+            this.employeeFormButton.FlatAppearance.BorderSize = 8;
+            this.employeeFormButton.Font = new System.Drawing.Font("Franklin Gothic Medium", 11.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.employeeFormButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
+            this.employeeFormButton.Location = new System.Drawing.Point(12, 234);
+            this.employeeFormButton.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.employeeFormButton.Name = "employeeFormButton";
+            this.employeeFormButton.Size = new System.Drawing.Size(776, 70);
+            this.employeeFormButton.TabIndex = 2;
+            this.employeeFormButton.Text = "Employee Form";
+            this.employeeFormButton.UseVisualStyleBackColor = true;
+            // 
+            // jobFormButton
+            // 
+            this.jobFormButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
+            this.jobFormButton.FlatAppearance.BorderSize = 8;
+            this.jobFormButton.Font = new System.Drawing.Font("Franklin Gothic Medium", 11.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.jobFormButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
+            this.jobFormButton.Location = new System.Drawing.Point(12, 314);
+            this.jobFormButton.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.jobFormButton.Name = "jobFormButton";
+            this.jobFormButton.Size = new System.Drawing.Size(776, 70);
+            this.jobFormButton.TabIndex = 3;
+            this.jobFormButton.Text = "Job Form";
+            this.jobFormButton.UseVisualStyleBackColor = true;
+            // 
+            // flightFormButton
+            // 
+            this.flightFormButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
+            this.flightFormButton.FlatAppearance.BorderSize = 8;
+            this.flightFormButton.Font = new System.Drawing.Font("Franklin Gothic Medium", 11.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.flightFormButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
+            this.flightFormButton.Location = new System.Drawing.Point(12, 394);
+            this.flightFormButton.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.flightFormButton.Name = "flightFormButton";
+            this.flightFormButton.Size = new System.Drawing.Size(776, 70);
+            this.flightFormButton.TabIndex = 4;
+            this.flightFormButton.Text = "Flight Form";
+            this.flightFormButton.UseVisualStyleBackColor = true;
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.Font = new System.Drawing.Font("Franklin Gothic Medium", 24F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(152)))), ((int)(((byte)(196)))), ((int)(((byte)(224)))));
+            this.titleLabel.Location = new System.Drawing.Point(12, 76);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(567, 39);
+            this.titleLabel.TabIndex = 6;
+            this.titleLabel.Text = "PixisAir Flight Reservations  ";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // greenAccentLabel
+            // 
+            this.greenAccentLabel.Font = new System.Drawing.Font("Franklin Gothic Medium", 24F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.greenAccentLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(177)))), ((int)(((byte)(195)))), ((int)(((byte)(152)))));
+            this.greenAccentLabel.Location = new System.Drawing.Point(12, 115);
+            this.greenAccentLabel.Name = "greenAccentLabel";
+            this.greenAccentLabel.Size = new System.Drawing.Size(567, 38);
+            this.greenAccentLabel.TabIndex = 7;
+            this.greenAccentLabel.Text = "                                                       ";
+            this.greenAccentLabel.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // orangeAccentLabel
+            // 
+            this.orangeAccentLabel.Font = new System.Drawing.Font("Franklin Gothic Medium", 24F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.orangeAccentLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(180)))), ((int)(((byte)(154)))));
+            this.orangeAccentLabel.Location = new System.Drawing.Point(12, 153);
+            this.orangeAccentLabel.Name = "orangeAccentLabel";
+            this.orangeAccentLabel.Size = new System.Drawing.Size(567, 38);
+            this.orangeAccentLabel.TabIndex = 8;
+            this.orangeAccentLabel.Text = "                                                             ";
+            this.orangeAccentLabel.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // pinkAccentLabel
+            // 
+            this.pinkAccentLabel.Font = new System.Drawing.Font("Franklin Gothic Medium", 24F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pinkAccentLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(160)))), ((int)(((byte)(176)))));
+            this.pinkAccentLabel.Location = new System.Drawing.Point(12, 191);
+            this.pinkAccentLabel.Name = "pinkAccentLabel";
+            this.pinkAccentLabel.Size = new System.Drawing.Size(567, 38);
+            this.pinkAccentLabel.TabIndex = 9;
+            this.pinkAccentLabel.Text = "                                                                   ";
+            this.pinkAccentLabel.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("Franklin Gothic Medium", 11.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(12, 472);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(776, 35);
+            this.exitButton.TabIndex = 11;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Font = new System.Drawing.Font("Franklin Gothic Medium", 9F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // PixisAirMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
+            this.ClientSize = new System.Drawing.Size(800, 519);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.pinkAccentLabel);
+            this.Controls.Add(this.orangeAccentLabel);
+            this.Controls.Add(this.greenAccentLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.flightFormButton);
+            this.Controls.Add(this.jobFormButton);
+            this.Controls.Add(this.employeeFormButton);
+            this.Controls.Add(this.paperPlanePictureBox);
+            this.Controls.Add(this.menuStrip);
+            this.Font = new System.Drawing.Font("Franklin Gothic Medium", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip;
+            this.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.Name = "PixisAirMain";
+            this.Text = "PixisAir";
+            ((System.ComponentModel.ISupportInitialize)(this.paperPlanePictureBox)).EndInit();
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox paperPlanePictureBox;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem employeeFormToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem jobFormToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem flightFormToolStripMenuItem;
+        private System.Windows.Forms.Button employeeFormButton;
+        private System.Windows.Forms.Button jobFormButton;
+        private System.Windows.Forms.Button flightFormButton;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label greenAccentLabel;
+        private System.Windows.Forms.Label orangeAccentLabel;
+        private System.Windows.Forms.Label pinkAccentLabel;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
